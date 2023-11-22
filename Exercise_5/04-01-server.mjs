2f55c97a562ca0ae087b883ec3f485a270178d66
Christian Conte <138510785+Chris1998c@users.noreply.github.com>
2023-11-22 12:06:17 +0100

    Update 04-01-server.mjs

--- Exercise_5/04-01-server.mjs
+++ Exercise_5/04-01-server.mjs
@@ -8,7 +8,7 @@ const server = createServer((request, response) => {
   response.setHeader("Content-Type", "text/html");
 
   response.end(
-    "<html><body><h1>Buongiorno...</h1></body></html>"
+    "<html><body><h1>Welcome...</h1></body></html>"
   );
 });
 
